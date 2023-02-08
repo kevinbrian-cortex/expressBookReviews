9b0db05db338f1e92b3d5607bbd1a86c7daf4bcd
realkevinbrian <kevinbrian@cortex-moz.com>
2023-02-08 20:47:59 +0200

    Updating the code for the authentication mechanism:

--- final_project/index.js
+++ final_project/index.js
@@ -8,15 +8,40 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    //Write the authenication mechanism here
+
+    //auth
+
+    if (req.session.authorization) {
+
+        //get token
+        const token = req.user.authorization['accessToken'];
+
+        //verify token
+        jwt.verify(token, 'access', (err, user) => {
+
+            //no errors
+            if (!err) {
+                req.user = user;
+                next();
+            } else {
+                return res.status(403).json({ message: "User not authorized" })
+            }
+        })
+
+    } else {
+        //error
+        return res.status(403).json({ message: "User not Logged In" })
+    }
+
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
