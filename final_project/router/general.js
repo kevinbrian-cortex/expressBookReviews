ad6518eb02f7c05e889d63d50e98dde9315321a7
realkevinbrian <kevinbrian@cortex-moz.com>
2023-02-08 20:51:56 +0200

    Task 1: list of books available in the shop

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,7 +13,8 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const allBooks = JSON.stringify(books);
+  return res.status(300).json(allBooks);
 });
 
 // Get book details based on ISBN
