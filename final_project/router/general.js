46b162e1cdae1128531dc552d188e6d11fbb8f50
realkevinbrian <kevinbrian@cortex-moz.com>
2023-02-08 21:28:09 +0200

    Task 5: Complete the code for getting book reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -83,7 +83,12 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const isbn = req.params.isbn;
+
+  const reviews = books[isbn].reviews;
+
+  return res.status(300).json({reviews});
 });
 
 module.exports.general = public_users;
